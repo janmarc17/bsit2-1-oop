a06eba7630e053dd79d1c9e9655ec7665ac007dd
janmarc17 <jlumacang91235@liceo.edu.ph>
2025-10-02 11:26:05 +0800

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public abstract class User {
     private String userId;
@@ -14,17 +13,9 @@ public abstract class User {
         this.borrowedItems = new ArrayList<>();
     }
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
+    public String getUserId() { return userId; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
 
     public int getBorrowedItemsCount() {
         return borrowedItems.size();
@@ -40,12 +31,11 @@ public abstract class User {
 
     public void displayBorrowedItems() {
         if (borrowedItems.isEmpty()) {
-            System.out.println(name + " has no borrowed items.");
-            return;
-        }
-        System.out.println(name + "'s borrowed items:");
-        for (LibraryItem it : borrowedItems) {
-            System.out.println("  - " + it.getItemInfo());
+            System.out.println("  None");
+        } else {
+            for (LibraryItem li : borrowedItems) {
+                System.out.println("  " + li.getItemInfo());
+            }
         }
     }
 
